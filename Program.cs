b4d71d706a3f4a246dafe0be9daba76285ae804f
xzehrax <rabiazehraunal@hotmail.com>
2022-10-18 15:35:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Eşkenar üçreginin kenar uzunluğunu giriniz");
+        int kenar = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i <= kenar; i++)
+        {
+            for (int j = 0; j < kenar+i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < kenar-i; k++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+            
+
+        }
+
+    }
+}
